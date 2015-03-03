247b7805423f74daf39ce70601f1b625b6da4e88
Ron Park and Tyler Whisett <whitsett.tyler@gmail.com>
2015-03-03 15:23:48 -0800

    Add hashtag element into the Tweet object

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,15 +1,18 @@
 Twitter.TweetsController = Ember.ArrayController.extend({
+
   sortProperties: ['date'],
   sortAscending: false,
 
   actions: {
 
     save: function() {
+      var myRe = /#\w+/g;
       var newTweet = this.store.createRecord('tweet', {
         tweet: this.get('tweet'),
-        date: new Date()
+        date: new Date(),
+        hashtag: myRe.exec(this.get('tweet'))
       });
-
+      debugger;
       newTweet.save();
       var self = this;
       self.set('tweet', "");
--- models/tweet.js
+++ models/tweet.js
@@ -1,5 +1,8 @@
 Twitter.Tweet = DS.Model.extend({
   tweet: DS.attr(),
   date: DS.attr(),
+  hashtag: DS.attr(),
   comments: DS.hasMany('comments', {async: true})
 });
+
+// var myRe = /#\w+/g;
